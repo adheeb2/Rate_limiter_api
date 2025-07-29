bfe2cce992b470bec8939520ee1b8548feedb6ed
adheeb2 <adheebanvar2@gmail.com>
2025-07-29 22:12:54 +0530

    feat():added rate limiting service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { timestamp } from 'rxjs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+}
+@Injectable()
+export class RateLimitingService {
+  private requestsMap = new Map<string, number[]>();
+  private readonly limit = 3;
+  private readonly windowsMs = 10_000;
+
+  isAllowed(userId: string): boolean {
+    const now = Date.now();
+    const timestamps = this.requestsMap.get(userId) || [];
+
+    const recent = timestamps.filter((ts) => now - ts <= this.windowsMs);
+
+    if (recent.length < this.limit) {
+      recent.push(now);
+      this.requestsMap.set(userId, recent);
+      return true;
+    } else {
+      return false;
+    }
   }
 }
